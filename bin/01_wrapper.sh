b19d9c7998a5e248c2e6739135315d46cafdf7df
rregiar <rreggiar@ucsc.edu>
2021-12-17 12:58:07 -0800

    re did quant wrapper to for loop

--- bin/01_wrapper.sh
+++ bin/01_wrapper.sh
@@ -2,42 +2,24 @@
 
 # array of jobs to execute the entire project's rna quant and qc workload
 
-nohup ./01_trimAndSalmon.sh /public/groups/kimlab/exRNA_disease_biomarkers/data/input_data/bioIvt_ctrl/ \
-	exo \
-	/public/groups/kimlab/indexes/sel.align.gencode.v38.salmon.v1.6.0.sidx \
-	/public/groups/kimlab/exRNA_disease_biomarkers/data/output_data/rna_qc \
-	2>&1 > /public/groups/kimlab/exRNA_disease_biomarkers/tmp/logs/bioIvt_ctrl_quant_log.txt &
+projectDir=$1 # /public/groups/kimlab/exRNA_disease_biomarkers/
+indexType=$2 # ucsc.rmsk.salmon
+index=`ls -d /public/groups/kimlab/indexes/*.v38.$indexType.v1.6.0.sidx`
 
-nohup ./01_trimAndSalmon.sh /public/groups/kimlab/exRNA_disease_biomarkers/data/input_data/bioIvt_covid \
-	exo \
-	/public/groups/kimlab/indexes/sel.align.gencode.v38.salmon.v1.6.0.sidx \
-	/public/groups/kimlab/exRNA_disease_biomarkers/data/output_data/rna_qc \
-	2>&1 > /public/groups/kimlab/exRNA_disease_biomarkers/tmp/logs/bioIvt_covid_quant_log.txt &
+echo $index
 
-nohup ./01_trimAndSalmon.sh /public/groups/kimlab/exRNA_disease_biomarkers/data/input_data/bioIvt_luad \
-	exo \
-	/public/groups/kimlab/indexes/sel.align.gencode.v38.salmon.v1.6.0.sidx \
-	/public/groups/kimlab/exRNA_disease_biomarkers/data/output_data/rna_qc \
-	2>&1 > /public/groups/kimlab/exRNA_disease_biomarkers/tmp/logs/bioIvt_luad_quant_log.txt &
+for inputDataPath in $projectDir/data/input_data/*; do
+	
+	adapterType='exo'
+	
+	if [[ $inputDataPath =~ 'intra' ]]; then adapterType='intra'; fi
 
+	nohup ./01_trimAndSalmon.sh $inputDataPath \
+		$adapterType \
+		$index \
+		$projectDir/data/output_data/rna_qc/$indexType \
+		2>&1 > $projectDir/tmp/logs/$(basename $inputDataPath)_"$indexType"_quant_log.txt &
 
-nohup ./01_trimAndSalmon.sh /public/groups/kimlab/exRNA_disease_biomarkers/data/input_data/bioIvt_panc \
-	exo \
-	/public/groups/kimlab/indexes/sel.align.gencode.v38.salmon.v1.6.0.sidx \
-	/public/groups/kimlab/exRNA_disease_biomarkers/data/output_data/rna_qc \
-	2>&1 > /public/groups/kimlab/exRNA_disease_biomarkers/tmp/logs/bioIvt_panc_quant_log.txt &
+done
 
-
-nohup ./01_trimAndSalmon.sh /public/groups/kimlab/exRNA_disease_biomarkers/data/input_data/panc1_intra/ \
-	intra \
-	/public/groups/kimlab/indexes/sel.align.gencode.v38.salmon.v1.6.0.sidx \
-	/public/groups/kimlab/exRNA_disease_biomarkers/data/output_data/rna_qc \
-	2>&1 > /public/groups/kimlab/exRNA_disease_biomarkers/tmp/logs/panc1_intra_quant_log.txt &
-
-
-nohup ./01_trimAndSalmon.sh /public/groups/kimlab/exRNA_disease_biomarkers/data/input_data/pittsburgh_ctrl \
-	exo \
-	/public/groups/kimlab/indexes/sel.align.gencode.v38.salmon.v1.6.0.sidx \
-	/public/groups/kimlab/exRNA_disease_biomarkers/data/output_data/rna_qc \
-	2>&1 > /public/groups/kimlab/exRNA_disease_biomarkers/tmp/logs/pittsburgh_ctrl_quant_log.txt &
 

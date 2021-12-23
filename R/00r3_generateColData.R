a6f2d1cc32f1aedd2d72b63f485869f8d9758036
rregiar <rreggiar@ucsc.edu>
2021-12-22 22:08:06 -0800

    testing new parsing

--- R/00r3_generateColData.R
+++ R/00r3_generateColData.R
@@ -20,16 +20,18 @@ library(SummarizedExperiment)
 library(HDF5Array)
 })
 
-parse.input <- function(data.path, output.name){
-    ## assembles metadata object from input data
+parse.input <- function(data_path, output_name, gencode_ver) {
 
-    files <- dir(file.path(data.path))
+	files <- dir(file.path(data_path))
+	print('files')
+	print(files)
+	output_files <- list.files(data_path, full.names = TRUE)
+    	names(output_files) <- output.files
 
-    output.files <- list.files(data.path, full.names = TRUE)
-    names(output.files) <- output.files
+    	print('output.files:')
+    	print(output_files)
 
-    print('output.files:')
-    print(output.files)
+	quit()
     
     # creates empty df to fill with sample info
     sample.info <- data.frame('files' = file.path(),
@@ -40,8 +42,8 @@ parse.input <- function(data.path, output.name){
 
     # iteratively add to the empyty df the parsed sample names
     # here with format 'condition.rep'
-    lapply(output.files, function(x){
-		   x.path <- Sys.glob(file.path(x,paste0("*v35.",output.name,"*"), "quant.sf"))[1]
+    lapply(output_files, function(x){
+		   x.path <- Sys.glob(file.path(x,paste0("*v", gencode_ver, ".",output_name,"*"), "quant.sf"))[1]
 		   x <- basename(x)
 		   sample.split <- str_split(x, '[.]', n=4)[[1]]
 		   condition <- sample.split[1]
@@ -67,70 +69,80 @@ parse.input <- function(data.path, output.name){
     print(sample.info.df)
 }
 
+build.tximeta.obj <- function() {
 
-paths.in <- scan(file=file("stdin", "r"), what="character", n=4)
-data.path <- paths.in[1]
-txome.path <- paths.in[2]
-output.name <- paths.in[3]
-txome.tsv <- paths.in[4]
-outpath <- paths.in[5]
-
-outpath <- file.path("/public/groups/kimlab/pittsburgh_pah_exoRNA/output.data/")
+	if (output_name == 'ucsc.rmsk.salmon') {
+		metaSkip = TRUE
+		print('import data with tximeta')
+		txi <- tximeta::tximeta(coldata = sample.info.df, type = 'salmon', skipMeta=metaSkip)
 
 
-print('parse input dir')
-parse.input(data.path, output.name)
+		print('save tx h5')
+		saveHDF5SummarizedExperiment(txi, dir=paste0(outpath,paste0(output_name, '_h5_se')) , replace=TRUE)
 
-print('load tximeta')
-tximeta::loadLinkedTxome(txome.path)
+		gxi <- tximeta::tximeta(coldata = sample.info.df, 
+			type = 'salmon', 
+			skipMeta=metaSkip, 
+			txOut=FALSE, 
+			tx2gene=tx2gene)
 
-tx.to.gene <- read_csv('/public/groups/kimlab/genomes.annotations/gencode.35/gencode.v35.ucsc.rmsk.tx.to.gene.csv', col_names=F)
-
-
-if (output.name == 'ucsc.rmsk.salmon'){
-	metaSkip = TRUE
-	print('import data with tximeta')
-	txi <- tximeta::tximeta(coldata = sample.info.df, type = 'salmon', skipMeta=metaSkip)
+		print('save gene h5')
+		saveHDF5SummarizedExperiment(gxi, dir=paste0(outpath,paste0(output_name, '_gene_h5_se')), replace=TRUE)
 
 
-	print('save tx h5')
-	saveHDF5SummarizedExperiment(txi, dir=paste0(outpath,paste0(output.name, '_h5_se')) , replace=TRUE)
+	} else {
+		metaSkip = FALSE
+		print('import data with tximeta')
+		txi <- tximeta::tximeta(coldata = sample.info.df, type = 'salmon', skipMeta=metaSkip)
+		print('save tx h5')
+		saveHDF5SummarizedExperiment(txi, dir=paste0(outpath,paste0(output_name, '_h5_se')) , replace=TRUE)
 
-	gxi <- tximeta::tximeta(coldata = sample.info.df, 
-		type = 'salmon', 
-		skipMeta=metaSkip, 
-		txOut=FALSE, 
-		tx2gene=tx.to.gene)
+		gxi <- summarizeToGene(txi)
 
-	print('save gene h5')
-	saveHDF5SummarizedExperiment(gxi, dir=paste0(outpath,paste0(output.name, '_gene_h5_se')), replace=TRUE)
+		print('save gene h5')
+		saveHDF5SummarizedExperiment(gxi, dir=paste0(outpath,paste0(output_name, '_gene_h5_se')), replace=TRUE)
 
+		if (!is.null(txome_tsv)){
+			cg <- read.delim(txome_tsv, header = TRUE, as.is = TRUE)
+		
+			colnames(cg)[colnames(cg) == 'intron'] <- 'unspliced'
+			split.gxi <- tximeta::splitSE(gxi, cg, assayName = 'counts')
+		
+			saveHDF5SummarizedExperiment(split.gxi, 
+				dir=paste0(outpath,paste0(output_name, '_gene_split_h5_se')), 
+				replace=TRUE)
+		}
 
-} else{
-	metaSkip = FALSE
-	print('import data with tximeta')
-	txi <- tximeta::tximeta(coldata = sample.info.df, type = 'salmon', skipMeta=metaSkip)
-	print('save tx h5')
-	saveHDF5SummarizedExperiment(txi, dir=paste0(outpath,paste0(output.name, '_h5_se')) , replace=TRUE)
-
-	gxi <- summarizeToGene(txi)
-
-	print('save gene h5')
-	saveHDF5SummarizedExperiment(gxi, dir=paste0(outpath,paste0(output.name, '_gene_h5_se')), replace=TRUE)
-
-	if (!is.null(txome.tsv)){
-		cg <- read.delim(txome.tsv, header = TRUE, as.is = TRUE)
-	
-		colnames(cg)[colnames(cg) == 'intron'] <- 'unspliced'
-		split.gxi <- tximeta::splitSE(gxi, cg, assayName = 'counts')
-	
-		saveHDF5SummarizedExperiment(split.gxi, 
-			dir=paste0(outpath,paste0(output.name, '_gene_split_h5_se')), 
-			replace=TRUE)
 	}
-
 }
 
 
 
+main <- function() {
 
+	paths.in <- scan(file=file("stdin", "r"), what="character", n=6)
+
+	data_path <- paths.in[1] 
+	txome_path <- paths.in[2]
+	output_name <- paths.in[3]
+	txome_tsv <- paths.in[4]
+	outpath <- paths.in[5]
+	gencode_ver <- paths.in[6]
+
+	print('parse input dir')
+	parse.input(data_path, output_name, gencode_ver)
+
+	#print('load tximeta')
+	#tximeta::loadLinkedTxome(txome_path)
+
+	#print(paste0('load tx2gene for gencode v ', gencode_ver))
+	#tx2gene_path <- file.path('/public/groupls/kimlab/genomes.annotation/gencode.', gencode_ver, paste0('gencode.v', gencode_ver, '.ucsc.rmsk.tx2gene.csv'))
+
+	#tx2gene <- read_csv(tx2gene_path, col_names=F)
+	
+	#print('build tximeta object')
+	#build.tximeta.obj()
+
+}
+
+main()

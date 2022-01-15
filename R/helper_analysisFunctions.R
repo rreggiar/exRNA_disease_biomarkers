4e657c24309f7c7e7c765497a6043df1ee2e53d6
rregiar <rreggiar@ucsc.edu>
2022-01-14 20:03:05 -0800

    incorporating quant_meta_df returning list of df do I want a single df?

--- R/helper_analysisFunctions.R
+++ R/helper_analysisFunctions.R
@@ -49,16 +49,60 @@ load.tximeta.object.list <- function(reference, output_data.dir = here::here('da
 			     gxi <- loadHDF5SummarizedExperiment(dir=file.path(output_data_path, 
 									       paste0(sample, '_gene_h5_se')))
 
+
 			     if (reference == 'process.aware.salmon'){
 				     gxi.split <- loadHDF5SummarizedExperiment(dir=file.path(output_data_path,
 											     paste0(sample, 
 												    '_gene_split_h5_se')))
 				     return(list('txi' = txi, 'gxi' = gxi, 'gxi.split' = gxi.split)) 
 			     } else {
-				     return(list('txi' = txi, 'gxi' = gxi))
+				     return(list('txi' = txi, 'gxi' = gxi)) 
 			     }
 	       }
 	)
+}
+
+parse.tximeta.quant.metadata <- function(se_list, qc_data.dir = here::here('data/output_data/rna_qc')) {
+
+			     salmon_quant_cols_of_interest <-
+					  c("frag_length_mean", "frag_length_mean", "frag_length_sd",
+					    "num_processed", "num_mapped", "num_decoy_fragments",
+					    "num_dovetail_fragments", "num_fragments_filtered_vm", "num_alignments_below_threshold_for_mapped_fragments_vm",
+					    "percent_mapped")
+
+			     imap(se_list, function(data, project) {
+
+					  meta_obj <- data$gxi@metadata$quantInfo
+					  names <- data$gxi$names
+
+					  meta_obj[names(meta_obj) %in% tximeta_quant_cols_of_interest] %>%
+					    as.data.frame() -> salmon_meta_tmp
+
+					  colnames(salmon_meta_tmp) <- paste0('salmon_', colnames(salmon_meta_tmp))
+
+					  salmon_meta_tmp %>% mutate(sample = names) -> salmon_meta_tmp
+
+					  project <- sub('_salmon', '', project)
+
+					  star_meta_tmp <- read_tsv(file.path(qc_data.dir,
+								    'star',
+								    paste0('multiqc.star.', project, '.2021-12-20-22.28.04-PST_data'),
+								    'multiqc_star.txt'))
+
+					  colnames(star_meta_tmp) <- paste0('star_', colnames(star_meta_tmp))
+
+					  star_meta_tmp %>%
+					    dplyr::rename('sample' = star_Sample) %>%
+					    mutate(sample = sub('_second_pass_out', '', sample)) -> star_meta_tmp
+
+					  meta_out <- merge(star_meta_tmp, salmon_meta_tmp, by = 'sample')
+
+					  meta_out
+
+					}
+			) -> meta_df
+
+	meta_df
 }
 
 subset.tximeta.object <- function(tximeta.list, grep.string){

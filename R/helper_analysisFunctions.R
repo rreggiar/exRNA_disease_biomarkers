74948d29d01d56f0ea18c198a3fc766540a051cc
rregiar <rreggiar@ucsc.edu>
2022-04-11 12:16:35 -0700

    remove axis label hiding from pca plot 1

--- R/helper_analysisFunctions.R
+++ R/helper_analysisFunctions.R
@@ -596,12 +596,11 @@ run.pca <- function(input_de,
     geom_point(size = rel(2), alpha = 1) + 
     # scale_shape_manual(values = c(21,25)) +
     ylab(paste('PC1', round(cumsum(pcs.props)[1], digits = 3), sep = ' ')) +
-    # xlab(paste('PC2', round(cumsum(pcs.props)[2] - cumsum(pcs.props)[1], digits = 3), sep = ' ')) +
+    xlab(paste('PC2', round(cumsum(pcs.props)[2] - cumsum(pcs.props)[1], digits = 3), sep = ' ')) +
     geom_vline(xintercept = 0, linetype = 'dotted', alpha = 0.3, size = 0.25) +
     geom_hline(yintercept = 0, linetype = 'dotted', alpha = 0.3, size = 0.25) + 
     scale_color_manual(values = identity_color_pal) + 
     theme(axis.title.x = element_blank()) +
-    
     ylim(-200,200) -> pca_1v2.plt
   
   pca.out %>% 

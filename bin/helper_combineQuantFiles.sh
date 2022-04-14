a66a6cd1cac042d5dea1f62d084604bdd87524e5
rregiar <rreggiar@ucsc.edu>
2022-04-13 17:04:27 -0700

    add ex run comment

--- bin/helper_combineQuantFiles.sh
+++ bin/helper_combineQuantFiles.sh
@@ -3,6 +3,11 @@
 
 #inputPath='/public/groups/kimlab/seqData/PANC_TCGA_TE-AWARE_COUNTS/roman-panc/TCGA_PAAD_ControlledAccess_V1-0_DATA_edu_ucsc_kim_lab/quant.sf'
 
+#nohup ./helper_combineQuantFiles.sh \
+#'/public/groups/kimlab/seqData/PANC_TCGA_TE-AWARE_COUNTS/roman-panc/GTEx/pancreas/quant.sf' \
+#'GTEX-Q2AH-0926-SM-48TZK.quant.sf.gz' \
+#'/public/groups/kimlab/exRNA_disease_biomarkers/data/output_data/tcga_gtex/paad_gtex_count_matrix.tsv' &
+
 inputPath=$1
 inputQuant=$2
 outputPath=$3

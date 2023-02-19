d59b01f1af5f6ca4f582dbf2b58761df4b158b4b
rregiar <rreggiar@ucsc.edu>
2023-02-19 13:41:34 -0800

    trying to make proj dir so not in home for rproj

--- bin/helper_launch.rstudio.sh
+++ bin/helper_launch.rstudio.sh
@@ -14,17 +14,17 @@ PROJ=$2
 echo "for project: "${PROJ}" "
 
 # user & project specific mounts
-DATA="/public/groups/kimlab/"${PROJ}"/data:/home/"${USER}"/data"
-NOTEBOOKS="/public/groups/kimlab/"${PROJ}"/notebooks:/home/"${USER}"/notebooks"
-MANUSCRIPT="/public/groups/kimlab/"${PROJ}"/manuscript:/home/"${USER}"/manuscript"
-BIN="/public/groups/kimlab/"${PROJ}"/bin:/home/"${USER}"/bin"
-R="/public/groups/kimlab/"${PROJ}"/R:/home/"${USER}"/R"
-MYSQL="/public/groups/kimlab/"${PROJ}"/my_sql:/home/"${USER}"/my_sql"
+DATA="/public/groups/kimlab/"${PROJ}"/data:/home/"${USER}"/proj/data"
+NOTEBOOKS="/public/groups/kimlab/"${PROJ}"/notebooks:/home/"${USER}"/proj/notebooks"
+MANUSCRIPT="/public/groups/kimlab/"${PROJ}"/manuscript:/home/"${USER}"/proj/manuscript"
+BIN="/public/groups/kimlab/"${PROJ}"/bin:/home/"${USER}"/proj/bin"
+R="/public/groups/kimlab/"${PROJ}"/R:/home/"${USER}"/proj/R"
+MYSQL="/public/groups/kimlab/"${PROJ}"/my_sql:/home/"${USER}"/proj/my_sql"
 # rreggiar config file for RSTUDIO looks and behavior
-CONFIG="/public/home/"${USER}"/.rstudio_docker_config:/home/"${USER}"/.config/rstudio"
+CONFIG="/public/home/"${USER}"/.rstudio_docker_config:/home/"${USER}"/proj/.config/rstudio"
 # lab wide reference
-REFERENCE="/public/groups/kimlab/genomes.annotations/gencode.39:/home/"${USER}"/reference"
-DOT_GIT="/public/groups/kimlab/"${PROJ}"/.git:/home/"${USER}"/.git"
+REFERENCE="/public/groups/kimlab/genomes.annotations/gencode.39:/home/"${USER}"/proj/reference"
+DOT_GIT="/public/groups/kimlab/"${PROJ}"/.git:/home/"${USER}"/proj/.git"
 
 echo "making rstudio session hosted at 127.0.0.1:"${PORT}":8787 for "${USER}":"${USER_ID}""
 docker run --rm -p 127.0.0.1:"${PORT}":8787 -e DISABLE_AUTH=true \
